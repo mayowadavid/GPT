a6075cfd3c3c105803fe4dd3b2c435593dc6dde7
mayowadavid <mayowadavid100@gmail.com>
2023-01-05 00:28:15 +0100

    first commit

---
+++ pages/api/generate.js
@@ -0,0 +1,26 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = "";
+const generateAction = async (req, res) => {
+  // Run first prompt
+ // console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `you are a world class drummer you know everything about drums only tell facts and if you don't know apologize and tell the person you dont know. if its not about drums tell the person you can only answer drumming related questions. ${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
